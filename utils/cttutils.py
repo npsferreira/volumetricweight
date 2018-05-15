349f3e3eb32050ce4c4b5c9ae0a2db9950ce0a7f
Nuno Silva <nunos@microsoft.com>
2018-05-15 17:48:32 +0100

    added utils

---
+++ utils/cttutils.py
@@ -0,0 +1,102 @@
+# Import the necessary packages
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+from urllib.request import urlopen
+
+def translate(image, x, y):
+	# Define the translation matrix and perform the translation
+	M = np.float32([[1, 0, x], [0, 1, y]])
+	shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+
+	# Return the translated image
+	return shifted
+
+def rotate(image, angle, center=None, scale=1.0):
+	# Grab the dimensions of the image
+	(h, w) = image.shape[:2]
+
+	# If the center is None, initialize it as the center of
+	# the image
+	if center is None:
+		center = (w // 2, h // 2)
+
+	# Perform the rotation
+	M = cv2.getRotationMatrix2D(center, angle, scale)
+	rotated = cv2.warpAffine(image, M, (w, h))
+
+	# Return the rotated image
+	return rotated
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+	# initialize the dimensions of the image to be resized and
+	# grab the image size
+	dim = None
+	(h, w) = image.shape[:2]
+
+	# if both the width and height are None, then return the
+	# original image
+	if width is None and height is None:
+		return image
+
+	# check to see if the width is None
+	if width is None:
+		# calculate the ratio of the height and construct the
+		# dimensions
+		r = height / float(h)
+		dim = (int(w * r), height)
+
+	# otherwise, the height is None
+	else:
+		# calculate the ratio of the width and construct the
+		# dimensions
+		r = width / float(w)
+		dim = (width, int(h * r))
+
+	# resize the image
+	resized = cv2.resize(image, dim, interpolation = inter)
+
+	# return the resized image
+	return resized
+
+def plot_histogram(image, title, mask=None):
+    # Grab the image channels, initialize the tuple of colors
+	# and the figure
+
+	sns.set()
+
+	chans = cv2.split(image)
+	colors = ("b", "g", "r")
+	plt.figure()
+	plt.title(title)
+	plt.xlabel("Bins")
+	plt.ylabel("# of Pixels")
+
+	# Loop over the image channels
+	for (chan, color) in zip(chans, colors):
+		# Create a histogram for the current channel and plot it
+		hist = cv2.calcHist([chan], [0], mask, [256], [0, 256])
+		plt.plot(hist, color = color)
+		plt.xlim([0, 256])
+
+def displayImage(image, figSize=(8,8), title=None):
+    fig = plt.figure(figsize=figSize)
+    plt.axis("off")
+    if len(image.shape) == 3:
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    else:
+        plt.imshow(image, cmap='Greys_r',  interpolation='bicubic')
+    if title is not None:
+        plt.title(title)
+    plt.show()
+
+def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
+    # download the image, convert it to a NumPy array, and then read
+    # it into OpenCV format
+    resp = urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, readFlag)
+
+    # return the image
+    return image
